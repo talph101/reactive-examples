21c3abe19fa700abb780b9372aec61cc04724e1b
Tiffany <139176913+talph101@users.noreply.github.com>
2025-01-29 10:50:02 -0600

    initial commit

---
+++ src/main/java/guru/springframework/reactive_examples/ReactiveExamplesApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.reactive_examples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/reactive_examples/ReactiveExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.reactive_examples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
